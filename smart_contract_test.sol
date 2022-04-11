d0e92b6c6b7b28cb2fba01165cb666fbc45715e7
Turki Alahmadi <71497457+TurkiAlahmadi@users.noreply.github.com>
2022-04-12 01:59:20 +0200

    Update smart_contract_test.sol
    added addGame function and updated joibGame function

--- smart_contract_test.sol
+++ smart_contract_test.sol
@@ -2,60 +2,57 @@
 pragma solidity >=0.7.0 <0.9.0;
 contract Four_In_A_Chain {
 
-
-
-
 	/// EVENTS ///
 	event GameStarted(address indexed player1, address indexed player2);
-
-    event Error(address indexed player1, string error);
-
+    event Error(address player1, string error);
 	///event MovePerformed(address indexed mover, uint256 gameId, uint8 row);
-
 	///event GameWon(address indexed winner, uint256 gameId);
 
+    uint256 public gameCount =0;
+    uint constant bet = 2 ether;
+    enum State {Initiated, InProgress, Ended}
+    mapping(uint => Game) games;
+
     uint public lastBlock;
-    uint public gameID;
-    uint constant bet = 2;
 
     modifier BetAmount {
-        if(msg.value != bet) {
-            emit Error(msg.sender, "Betting amount is 2 ether");
-        }
+        require(msg.value == bet, "Betting amount is 2 ether");
         _;
     }
 
     struct Game {
+        uint gameID;
 		address player1;
 		address player2;
-        bool NoGameInitiated;
-        bool GameInitiated;
-		bool GameInProgress;
-        bool GameEnded;
         uint8[6][7] board;
+        State gameState;
     }
-
-    mapping(uint => Game) games;
  
     ///function getBoard(int ind) public view returns (uint8[6][7]) {
     ///    return games[ind].board;
     ///    }
 
-    function joinGame() public payable BetAmount returns (uint ind, address p1, address p2) {
-        Game storage g = games[gameID];
-        if (g.GameInitiated == false){
-            g.player1 = msg.sender;
-            g.GameInitiated == true;
-        }
-        else {
-            require(msg.sender != g.player1, "You can't play against yourself!");
-            g.player2 = msg.sender;
-            g.GameInProgress = true;
-            emit GameStarted(g.player1,g.player2);
-            gameID++;
-            return (gameID, g.player1, g.player2);
-        }
-        
+    function addGame(address p1, address p2) public {
+        gameCount++;
+        uint8[6][7] memory board;
+        State gameState;
+        gameState = State.Initiated;
+        games[gameCount] = Game(gameCount, p1, p2, board, gameState);
     }
 
+    function joinGame() public payable BetAmount {
+        Game storage g = games[gameCount];
+        if (gameCount == 0) {
+            addGame(msg.sender, address(0));
+        }
+        else if (g.gameState == State.InProgress || g.gameState == State.Ended) {
+            addGame(msg.sender, address(0));
+        }
+        else if (g.gameState == State.Initiated) {
+            require(msg.sender != g.player1, "You can't play against yourself!");
+            g.player2 = msg.sender;
+            g.gameState = State.InProgress;
+            emit GameStarted(g.player1,g.player2);
+        }
+    }
 }
