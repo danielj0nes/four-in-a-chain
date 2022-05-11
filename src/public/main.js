950da218f84db0b1b25918538e8806b1f27197c6
Davide Busolin <davide.busolin@studenti.unimi.it>
2022-05-11 12:19:42 +0200

    Subscribe trial, wasn't successful

--- src/public/main.js
+++ src/public/main.js
@@ -31,6 +31,44 @@ const init = async () => {
     gameContract = new web3.eth.Contract(gameAbi, "0x39E19305b3314dbE8CC521f0d9369fA113b0D1bc");
     initialised = true;
     console.log(gameContract);
+
+    /*
+    //appears to list past events until the moment of the button click
+    gameContract.events.GameStarted({
+        fromBlock: 0
+    }, function(error, event){ console.log(event); })
+    //.on('data', function(event){
+    //    console.log(event);
+    //})
+    .on('error', console.error);
+
+    const eventEvents = () => {
+        console.log(`Listening Transfer events`);
+        contractInstance.events
+            .Transfer()
+            .on("data", (event) => console.log(event))
+            .on("error", (error) => console.log(error));
+    }
+    eventEvents();
+    */
+
+    //From https://parv3213.medium.com/understanding-web3-events-6524c06b669f
+    const eventEvents = () => {
+        console.log(`Listening GameStarted events`);
+        gameContract.events
+            .GameStarted()
+            .on("data", (event) => console.log(event))
+            .on("error", (error) => console.log(error));
+    }
+    eventEvents();
+
+    const eventOnce = () => {
+        gameContract.once("GameStarted", (error, event) => {
+            if(!error) console.log(event);
+            else console.log(`Error: ${error}`);
+        })
+    }
+    eventOnce();
 }
 
 const testContract = async () => {
@@ -38,18 +76,12 @@ const testContract = async () => {
         await init();
     }
     console.log(gameContract.methods);
-
-    /*
-    let subscription = web3.eth.subscribe('GameStarted', {}, function(error, result){
-        if (!error)
-            console.log(result);
-    });
-    */
+    
 
     var returnvalue = await gameContract.methods.joinGame().send({from: selectedAccount, gas: "200000", value:"2000000000000000000"}).on("receipt", function(receipt){
         console.log(receipt)
         }).on('error', function(error, receipt) {
             console.log(error);
         });
-    console.log(returnvalue) //seems to be same as receipt
+    //console.log(returnvalue) //seems to be same as receipt
 };
