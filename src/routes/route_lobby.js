7ea2807c7681c4bb162f2406cdfa61e96a3ff955
Daniel <daniel@jones.ac>
2022-05-15 23:00:10 +0200

    added routes and basic logic for multiplayer, just need to integrate the contract now

--- src/routes/route_lobby.js
+++ src/routes/route_lobby.js
@@ -4,15 +4,36 @@
  * @author Daniel Jones
  */
 import Router from "koa-router";
-import Web3 from "web3";
-// import Storage from "../../contracts/testcontract_abi.json";
 
 const lobbyPrefix = "/lobby"
 const router = new Router({ prefix: lobbyPrefix });
+let playerInfo = {}
 
 router.get("/", async ctx => {
     await ctx.render("lobby", ctx.hbs);
 });
+
+// Terrible js incoming (why the fk does ctx.redirect/render not work?!)
+router.post("/opponent-search", async ctx => {
+    try {
+        let connectedWallet = ctx.request.body.walletId;
+        console.log(`Connection from: ${connectedWallet}`);
+
+        playerInfo[connectedWallet] = "connected";
+        if (Object.keys(playerInfo).length > 2) {
+            // Flush the json object
+            playerInfo = {}
+            playerInfo[connectedWallet] = "connected"
+        }
+    } catch (err) {
+        console.log(err);
+    }
+    
+})
+// Server the info
+router.get("/ready", async ctx => {
+    ctx.body = playerInfo;
+})
 
  export default router;
  
