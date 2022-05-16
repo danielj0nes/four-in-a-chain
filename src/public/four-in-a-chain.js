527f01947eb8f31e55727b1c3b5a9b63419813b4
Davide Busolin <davide.busolin@studenti.unimi.it>
2022-05-16 15:50:47 +0200

    checkAccountInGame

--- src/public/four-in-a-chain.js
+++ src/public/four-in-a-chain.js
@@ -7,6 +7,26 @@ fetch("four-in-a-chain_abi.json")
   .then(res => res.json())
   .then(obj => gameAbi = obj)
 
+const checkAccountInGame = async (provider) => {
+  fetch('/lobby/ready')
+      .then(res => res.json())
+      .then(data => {
+        // Once two wallets are connected
+        if (Object.keys(data).length === 2) {
+          let wallet1 = Object.keys(data)[0];
+          let wallet2 = Object.keys(data)[1];
+          if(selectedAccount != wallet1 && selectedAccount != wallet2){
+            alert("The account you selected is not in the lobby at the moment")
+          } else {
+            console.log("account lobby check passed")
+          }
+        } 
+
+        else { alert("You're not in a valid lobby at the moment") }
+          //redirect to lobby screen?
+      })
+}
+
 const init = async () => {
     let provider = window.ethereum;
     if (typeof provider !== "undefined") {
@@ -18,6 +38,7 @@ const init = async () => {
             walletDiv = document.getElementById('wallet-id');
             walletDiv.textContent = `${selectedAccount}`
             console.log(`selected account ${selectedAccount}`);
+            checkAccountInGame();
         })
         .catch((err) => {
             console.log(err);
@@ -28,6 +49,7 @@ const init = async () => {
         walletDiv = document.getElementById('wallet-id');
         walletDiv.textContent = `${selectedAccount}`
         console.log(`selected account changed to ${selectedAccount}`);
+        checkAccountInGame();
     });
     const web3 = new Web3(provider);
     // const networkId = await web3.eth.net.getId();
