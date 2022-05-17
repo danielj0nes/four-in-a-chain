0de5885abe70bfa89d88fa6ac07d1f031d86e2b3
Daniel <daniel@jones.ac>
2022-05-17 22:04:45 +0200

    Deploying the contract using the bytecode; MANY ISSUES SOLVEDexport GPG_TTY=/dev/pts/5export GPG_TTY=/dev/pts/5!

--- src/public/main.js
+++ src/public/main.js
@@ -3,9 +3,14 @@ let gameContract;
 let gameAbi;
 let initialised = false;
 let walletDiv;
-fetch("four-in-a-chain_abi.json")
+let gameByteCode;
+fetch("four-in-a-chain-bytecode.json")
   .then(res => res.json())
-  .then(obj => gameAbi = obj)
+  .then(obj => gameByteCode = obj)
+fetch("four-in-a-chain_abi.json")
+    .then(res => res.json())
+    .then(obj => gameAbi = obj)
+
 
 const init = async () => {
     let provider = window.ethereum;
@@ -23,24 +28,43 @@ const init = async () => {
             console.log(err);
         });
     }
+    // Disable this when debugging, causes lots of issues
+
     window.ethereum.on("accountsChanged", function(accounts) {
         selectedAccount = accounts[0];
         walletDiv = document.getElementById('wallet-id');
         walletDiv.textContent = `${selectedAccount}`
         console.log(`selected account changed to ${selectedAccount}`);
     });
+
     const web3 = new Web3(provider);
-    // const networkId = await web3.eth.net.getId();
+    // First create the instance of the game contract using the ABI
     gameContract = new web3.eth.Contract(gameAbi, selectedAccount);
+    // const networkId = await web3.eth.net.getId();
     initialised = true;
-    console.log(gameContract);
 }
 
-const testContract = async () => {
+const initGame = async () => {
+    // Create the deployed contract instance
     if (!initialised) {
         await init();
     }
-    console.log(gameContract.methods);
+    let contractInstance = await gameContract.deploy({
+        data: gameByteCode["object"]
+    })
+    .send({
+        from: selectedAccount
+    })
+    .then()
+    console.log(contractInstance.methods);
+    // Execute the initGame function of the contract
+    contractInstance.methods.initGame().send({
+        from: selectedAccount
+    })
+    .on("receipt", function(receipt) {
+        console.log(receipt);
+    })
+
     /*
     gameContract.methods.store("1").send({from: selectedAccount, gas: "200000"}).on("receipt", function(receipt){
         console.log(receipt)
